ed7a48fb0ff910796b30ee6cb2e5b98c0f22ae6a
Ian Hersom <ian.hersom@gmail.com>
2025-08-31 21:06:18 -0600

    add version

--- lua/remote-ssh.lua
+++ lua/remote-ssh.lua
@@ -1,8 +1,12 @@
 local M = {}
 
 function M.setup(opts)
+    local version = require("version")
+    print("Plugin version: " .. version.version)
+
     local remote_treesitter = require("remote-treesitter")
     local remote_lsp = require("remote-lsp")
+
     remote_lsp.setup(opts)
     remote_treesitter.setup()
 end
