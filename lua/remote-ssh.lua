3caf35ad6e571786c8b2ddfdb59d2e04e824c896
Ian Hersom <ian.hersom@gmail.com>
2025-10-29 16:30:53 -0600

    stop printing plugin version

--- lua/remote-ssh.lua
+++ lua/remote-ssh.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup(opts)
     local version = require("version")
-    print("Plugin version: " .. version.version)
+    -- print("Plugin version: " .. version.version)
 
     local remote_treesitter = require("remote-treesitter")
     local remote_lsp = require("remote-lsp")
